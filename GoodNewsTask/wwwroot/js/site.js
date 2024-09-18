d915cbb4e5b6f3580ac3b6849fc1faec20302dd9
Anatoliy <Radevich.Anatoliy@gmail.com>
2024-09-18 20:08:17 +0300

    Fixed bug in method ShowArticlesFromDBForGuests of DisplayController. Added buttons which change color scheme

--- GoodNewsTask/wwwroot/js/site.js
+++ GoodNewsTask/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Переключение между цветовыми решениями
+function selectMode(selectedByUserMode) { //функция выбора цветового режима
+
+    const modesArray = ["morningMode", "dayMode", "eveningMode", "nightMode"]; //инициализация массива с режимами
+
+    modesArray.forEach((anyMode) => {
+        document.body.classList.remove(anyMode);
+    });//удаление (методом remove()) всех активных (работающих) режимов (в нашем случае берётся body)
+
+    if (selectedByUserMode) {
+        document.body.classList.add(selectedByUserMode);
+    }//добавление (методом add()) выбранным пользователем режима и его запуск 
+}
+
+//Реализация каждого цветового режима под каждую кнопку
+function morningModeToggle() {
+    selectMode("morningMode");
+}
+
+function dayModeToggle() {
+    selectMode("dayMode");
+}
+
+function eveningModeToggle() {
+    selectMode("eveningMode");
+}
+function nightModeToggle() {
+    selectMode("nightMode");
+}
